7cdef50858ac755034f2d4933d4968afb2f31740
Thanendra <kashyapthanendra@gmail.com>
2023-12-27 14:59:48 +0530

    Day - 45

---
+++ Day-45/com/project/spring/core/autowire/bean/constructor/Address.java
@@ -0,0 +1,38 @@
+package com.project.spring.core.autowire.bean.constructor;
+
+public class Address {
+	private String city, country;
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(String city, String country) {
+		super();
+		this.city = city;
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", country=" + country + "]";
+	}
+
+}
